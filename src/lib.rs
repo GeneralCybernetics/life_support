cf9c0a198d40fd2b2b5e2d607ad3ab37d2e821c6
aydin <aydin@saronic.com>
2023-12-31 21:20:02 -0600

    initial motor driver code

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#![no_std]
+
+mod medium_replacement;
+
+pub use crate::medium_replacement::PeristalticPump;
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{OutputType, Output, Level, Speed};
-use embassy_stm32::time::khz;
+use embassy_time::Duration;
 use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::timer::Channel;
 use embassy_time::Timer;
+use LifeSupport::PeristalticPump;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
@@ -16,26 +17,20 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let pump_in_enable = Output::new(p.PC7, Level::High, Speed::High);
-    let pump_out_enable = Output::new(p.PC9, Level::Low, Speed::High);
+    let pump_1 = Output::new(p.PC7, Level::Low, Speed::High);
+    let pump_2 = Output::new(p.PC9, Level::Low, Speed::High);
+    let pump_enable = Output::new(p.PA9, Level::Low, Speed::High);
 
-    let mut pwm_in = SimplePwm::new(p.TIM1, Some(pump_in_ctl), None, None, None, khz(10), Default::default()); 
-    let mut pwm_out = SimplePwm::new(p.TIM3, None, Some(pump_out_ctl), None, None, khz(10), Default::default()); 
-
-
-    let max = pwm_in.get_max_duty();
-    pwm_in.enable(Channel::Ch1);
-    pwm_out.enable(Channel::Ch2);
-
-
-    info!("PWM initialized");
-    info!("PWM max duty {}", max);
+    let mut pump = PeristalticPump::new(pump_1, pump_2, pump_enable);
 
     loop {
-        pwm_in.set_duty(Channel::Ch1, 255);
-        pwm_out.set_duty(Channel::Ch2, 0);
-
-
-        loop {}
+        pump.start_in();
+        Timer::after(Duration::from_millis(1000)).await;
+        pump.stop();
+        Timer::after(Duration::from_millis(300)).await;
+        pump.start_out();
+        Timer::after(Duration::from_millis(1000)).await;
+        pump.stop();
+        Timer::after(Duration::from_millis(300)).await;
     }
 }
