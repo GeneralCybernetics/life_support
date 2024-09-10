edaa559df33a84f40b543572f89821aad6046088
virajcz <virajchhajed21@gmail.com>
2024-09-10 16:51:28 -0500

    drivers update

--- src/drivers/drv8871.rs
+++ src/drivers/drv8871.rs
@@ -1,6 +1,4 @@
-// use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
-
-use embassy_stm32::gpio::{Pin, Output};
+use embassy_stm32::gpio::Output;
 
 pub enum DRV8871State {
     Coast,
@@ -9,21 +7,19 @@ pub enum DRV8871State {
     Brake,
 }
 
-// TODO: move this to PWM pins for frequency control
-pub struct DRV8871<'a, Out1: Pin, Out2: Pin> {
-    out_1: Output<'a, Out1>,
-    out_2: Output<'a, Out2>,
-    pub state: DRV8871State
+pub struct DRV8871<'a> {
+    out_1: Output<'a>,
+    out_2: Output<'a>,
+    pub state: DRV8871State,
 }
 
-impl<'a, Out1: Pin, Out2: Pin> DRV8871<'a, Out1, Out2> {
-
-    pub fn new(out_1: Output<'a, Out1>, out_2: Output<'a, Out2>) -> DRV8871<'a, Out1, Out2> {
+impl<'a> DRV8871<'a> {
+    pub fn new(out_1: Output<'a>, out_2: Output<'a>) -> DRV8871<'a> {
         return DRV8871 {
             out_1,
             out_2,
-            state: DRV8871State::Coast
-        }
+            state: DRV8871State::Coast,
+        };
     }
 
     pub fn forward(&mut self) {
--- src/drivers/thermistor.rs
+++ src/drivers/thermistor.rs
@@ -1,5 +1,5 @@
+use embassy_stm32::adc::{Adc, AdcChannel, Instance, Resolution};
 use embassy_stm32::gpio::Pin;
-use embassy_stm32::adc::{AdcPin, Resolution, Adc, Instance};
 use libm::logf;
 
 const B_COEFFICIENT: f32 = 3950.0;
@@ -7,37 +7,41 @@ const T0_KELVIN: f32 = 298.15; // room temp in Kelvin
 const R_AT_T0: f32 = 10_000.0; // resistance of the thermistor at room temp
 const R_DIVIDER_FIXED: f32 = 10_000.0; // resistance of the fixed resistor in the voltage divider
 
-
-pub struct Thermistor<'a, AdcReadPin: Pin + AdcPin<AdcInstance>, AdcInstance: Instance> {
-
+pub struct Thermistor<'a, AdcReadPin: Pin + AdcChannel<AdcInstance>, AdcInstance: Instance> {
     adc_pin: AdcReadPin,
     adc: Adc<'a, AdcInstance>,
-    resolution: Resolution
-
+    resolution: Resolution,
 }
 
-impl<'a, AdcReadPin: Pin + AdcPin<AdcInstance>, AdcInstance: Instance> Thermistor<'a, AdcReadPin, AdcInstance> {
+impl<'a, AdcReadPin: Pin + AdcChannel<AdcInstance>, AdcInstance: Instance>
+    Thermistor<'a, AdcReadPin, AdcInstance>
+{
     pub fn new(adc_pin: AdcReadPin, mut adc: Adc<'a, AdcInstance>) -> Self {
-        
-        let resolution = Resolution::TwelveBit;
+        let resolution = Resolution::BITS12;
         adc.set_resolution(resolution);
-        
+
         Thermistor {
             adc_pin,
             adc,
-            resolution
+            resolution,
         }
     }
 
     fn get_resistance(&mut self) -> f32 {
-        let adc_reading = self.adc.read(&mut self.adc_pin) as f32;
-        let max_count = self.resolution.to_max_count() as f32;
+        let adc_reading = self.adc.blocking_read(&mut self.adc_pin) as f32;
+        let max_count = match self.resolution {
+            Resolution::BITS12 => ((1 << 12) - 1) as f32,
+            Resolution::BITS10 => ((1 << 10) - 1) as f32,
+            Resolution::BITS8 => ((1 << 8) - 1) as f32,
+            Resolution::BITS6 => ((1 << 6) - 1) as f32,
+        };
         R_DIVIDER_FIXED / (max_count / adc_reading - 1.0)
     }
 
     pub fn temp_c(&mut self) -> f32 {
         let resistance = self.get_resistance();
-        let temperature = 1.0 / (1.0 / T0_KELVIN + 1.0 / B_COEFFICIENT * logf(resistance / R_AT_T0));
+        let temperature =
+            1.0 / (1.0 / T0_KELVIN + 1.0 / B_COEFFICIENT * logf(resistance / R_AT_T0));
         temperature - 273.15
     }
 }
